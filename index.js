13914ca75a3a1f111d1565a06d07b6570b659f80
Daniel Zambelli <mail@danielzambelli.dk>
2022-06-12 15:04:41 +0200

    patch

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ const initDb = ({pgDb}) => {
   })
 }
 
-const initSubs = ({Db, schema, tablePrefix}) => {
+function initSubs(){
   const { Model, DataTypes } = require('sequelize')
   class Subscribers extends Model{}
   Subscribers.init({
     contact_id: { type: DataTypes.INTEGER, unique: 'unique_subscription', primaryKey: true },
     campaign_id: { type: DataTypes.STRING, unique: 'unique_subscription', primaryKey: true },
     active: { type: DataTypes.BOOLEAN, defaultValue: true },
-  },{ sequelize: Db, schema, modelName: tablePrefix+'subscribers', freezeTableName: true })
+  },{ sequelize: this.Db, schema: this.schema, modelName: this.tablePrefix+'subscribers', freezeTableName: true })
   return Subscribers
 }
 
-const initTracks = ({Db, schema, tablePrefix}) => {
+function initTracks(){
   const { Model, DataTypes } = require('sequelize')
   class Tracks extends Model{}
   Tracks.init({
@@ -34,40 +34,33 @@ const initTracks = ({Db, schema, tablePrefix}) => {
     contact_id: { type: DataTypes.INTEGER, allowNull: false },
     campaign_id: { type: DataTypes.STRING, allowNull: false },
     action_id: { type: DataTypes.STRING, allowNull: false },
-  },{ sequelize: Db, schema, modelName: tablePrefix+'tracks', freezeTableName: true })
+  },{ sequelize: this.Db, schema: this.schema, modelName: this.tablePrefix+'tracks', freezeTableName: true })
   return Tracks
 }
 
-const syncSchema = ({Subs, schema}) => {
-  return Subs.sequelize.createSchema(schema).catch(e => { if(!e.message.match(/already exists/ig)) throw e })
+function syncSchema(){
+  return this.Subs.sequelize.createSchema(this.schema).catch(e => { if(!e.message.match(/already exists/ig)) throw e })
 }
 
-async function getTriggeredSubscribers({ action }){
-  // identify qualified subscribers
+function getTriggeredSubscribers({ action }){
+
+  if(!['once','recurring'].includes(action.trigger.type))
+    throw new Error(`unsupported action trigger type ${action.trigger.type}`)
+
   const SubsTable = this.schema ? `${this.schema}.${this.Subs.tableName}` : this.Subs.tableName
   const tracksTable = this.schema ? `${this.schema}.${this.Tracks.tableName}` : this.Tracks.tableName
-  let sql = null
-  switch(action.trigger.type){
-    case 'once':
-      sql = `
-        select * from ${SubsTable} where active = true and "createdAt" < NOW() - INTERVAL '0 DAY' and campaign_id = '${this.id}' and contact_id not in (
-          select distinct contact_id from ${tracksTable} where campaign_id = '${this.id}' and action_id = '${action.id}'
-        );
-      `
-    break
-    // TODO: recuring
-    default:
-      throw new Error(`unsupported action trigger type ${action.trigger.type}`)
-  }
-  let [subs] = await this.Db.query(sql)
+  const sqlRecurring = action.trigger.type === 'recurring' ? `and "createdAt" >= NOW() - INTERVAL '${action.trigger.intervalDays} DAY'` : ''
+  sql = `
+    select * from ${SubsTable} where active = true and "createdAt" < NOW() - INTERVAL '0 DAY' and campaign_id = '${this.id}' and contact_id not in (
+      select distinct contact_id from ${tracksTable} where campaign_id = '${this.id}' and action_id = '${action.id}' ${sqlRecurring}
+    );
+  `
+  return this.Db.query(sql).then(([res]) => res)
+}
 
-  // enrich with contact data
-  const contacts = await this.getContacts(subs.map(sub => sub.contact_id))
-  for(const sub of subs){
-    if(contacts[sub.contact_id]) sub.contact = contacts[sub.contact_id]
-  }
-
-  return subs
+async function getContactsMap(contactIds){
+  const contacts = await this.getContacts(contactIds)
+  return contacts.reduce((map, contact) => ({ ...map, [contact[this.contactIdField]]: contact }), {})
 }
 
 module.exports = class CampaignJS{
@@ -77,29 +70,32 @@ module.exports = class CampaignJS{
     schema=undefined,
     tablePrefix='campaign_',
     getContacts,
+    contactIdField,
     intervalMs=1000 * 60 * 30, //every 30min
     id,
     callbacks={},
     actions=[],
   }){
     // validate input
-    if(!pgDb) throw new Error('pgDb required instantiate CampaignJS')
-    if(!getContacts) throw new Error('getContacts required instantiate CampaignJS')
+    if(!pgDb) throw new Error('pgDb required to instantiate CampaignJS')
+    if(!getContacts) throw new Error('getContacts required to instantiate CampaignJS')
+    if(!contactIdField) throw new Error('contactIdField required to instantiate CampaignJS')
     if(!id) throw new Error('id required to instantiate CampaignJS')
 
     // assign
     this.schema = schema
     this.tablePrefix = tablePrefix
     this.getContacts = getContacts
+    this.contactIdField = contactIdField
     this.intervalMs = intervalMs
     this.id = id
     this.callbacks = callbacks
     this.actions = actions
 
     // init
-    this.Db = initDb({ pgDb })
-    this.Subs = initSubs({ Db: this.Db, schema: this.schema, tablePrefix: this.tablePrefix })
-    this.Tracks = initTracks({ Db: this.Db, schema: this.schema, tablePrefix: this.tablePrefix })
+    this.Db = initDb({pgDb})
+    this.Subs = initSubs.call(this)
+    this.Tracks = initTracks.call(this)
   }
 
   async start(){
@@ -115,7 +111,7 @@ module.exports = class CampaignJS{
   }
 
   async sync(force=false){
-    if(this.schema) await syncSchema({ Subs: this.Subs, schema: this.schema })
+    if(this.schema) await syncSchema.call(this)
     await Promise.all([ this.Subs.sync({ force }), this.Tracks.sync({ force }) ])
   }
 
@@ -146,11 +142,26 @@ module.exports = class CampaignJS{
     return this.Subs.update({ active: false }, { where: { contact_id: contactId, campaign_id: this.id } })
   }
 
+  getSubscribedContactIds(){
+    return this.Subs.findAll({ where: { campaign_id: this.id, active: true }}).then(res => res.map(sub => sub.contact_id))
+  }
+
   async process(skipIntervals=true){
     if(!skipIntervals) this.stop()
     for(const action of this.actions){
       const subscribers = await getTriggeredSubscribers.call(this, { action })
+
+      // map and join
+      const contacts = await getContactsMap.call(this, subscribers.map(sub => sub.contact_id))
+      for(const sub of subscribers){
+        if(contacts[sub.contact_id])
+          sub.contact = contacts[sub.contact_id]
+      }
+
+      // process action
       for(const subscriber of subscribers){
+        // TODO: pass more infor
+        // TODO: test that the correct contact and info is passed
         await this.callbacks[action.callback](subscriber.contact, action.params)
         await this.Tracks.create({ contact_id: subscriber.contact_id, campaign_id: this.id, action_id: action.id })
       }
@@ -158,15 +169,3 @@ module.exports = class CampaignJS{
     if(!skipIntervals) await this.start()
   }
 }
-
-
-// {
-//   id: 'email_linkedin_1',
-//   triggers: { type: 'recurring', intervalDays: 15 },
-//   action: { type: 'sendEmail', contentId: 'email_linkedin_category' },
-// },
-// {
-//   id: 'email_linkedin_2',
-//   triggers: { type: 'recurring', interval: '1 days', after: 'email_linkedin_1', callback: 'NotFriendsOnLinkedIn' },
-//   action: { type: 'sendEmail', contentId: 'email_linkedin_not_friends_category' },
-// }
--- index.test.js
+++ index.test.js
@@ -1,18 +1,20 @@
+const moment = require('moment')
 const ClientClass = require('./index')
 
 const contacts = {
-  1: { id: 1, email: 'julie@gmail.com', name: 'Julie Doe' },
-  2: { id: 2, email: 'jane@gmail.com', name: 'Jane Doe' },
-  3: { id: 3, email: 'jenny@gmail.com', name: 'Jenny Doe' },
-  4: { id: 4, email: 'jenice@gmail.com', name: 'Jenice Doe' },
+  1: { uid: 1, email: 'julie@gmail.com', name: 'Julie Doe' },
+  2: { uid: 2, email: 'jane@gmail.com', name: 'Jane Doe' },
+  3: { uid: 3, email: 'jenny@gmail.com', name: 'Jenny Doe' },
+  4: { uid: 4, email: 'jenice@gmail.com', name: 'Jenice Doe' },
 }
 
 const options = {
+  interval: 2000,
   pgDb: process.env.PGDB,
   schema: 'campaignjs_test',
   tablePrefix: 'campaign_',
-  getContacts: (contactIds) => contactIds.reduce((map,contactId) => ({ ...map, [contactId]: contacts[contactId] }), {}),
-  interval: 2000,
+  getContacts: (contactIds) => contactIds.map(id => contacts[id]),
+  contactIdField: 'uid',
 }
 
 describe(ClientClass.name, () => {
@@ -31,42 +33,6 @@ describe(ClientClass.name, () => {
 
   it('should be defined', () => {
     expect(ClientClass).toBeDefined()
-  })
-
-  it('should trigger once', async () => {
-    const res1 = []
-    const client = new ClientClass({ ...options, id: 'test_once_1',
-      callbacks: { testCallback: (contact, params) => res1.push({contact, params}) },
-      actions:[{ id: 'action', callback: 'testCallback', params: { msg: 'hello world' }, trigger: { type: 'once' } }]
-    })
-    await client.subscribe([1,2,3])
-    await client.unsubscribe(3)
-    await client.Tracks.bulkCreate([{ contact_id: 2, campaign_id: client.id, action_id: 'action' }])
-    await client.process()
-    const res2 = await client.Tracks.findAll({ attributes: ['contact_id', 'campaign_id', 'action_id'], where: { campaign_id: client.id } }).then(res => res.map(res => res.toJSON()))
-    await client.Subs.destroy({ where: { campaign_id: client.id } })
-    await client.Tracks.destroy({ where: { campaign_id: client.id } })
-    expect(res1).toEqual([
-      { contact: { id: 1, email: 'julie@gmail.com', name: 'Julie Doe' }, params: { msg: 'hello world' } }
-    ])
-    expect(res2).toEqual([
-      { contact_id: 2, campaign_id: 'test_once_1', action_id: 'action' },
-      { contact_id: 1, campaign_id: 'test_once_1', action_id: 'action' }
-    ])
-  })
-
-  it('should trigger once - considering createdAt', async () => {
-    const client = new ClientClass({ ...options, id: 'test_once_2',
-      callbacks: { testCallback: (contact, params) => null },
-      actions:[{ id: 'action', callback: 'testCallback', params: null, trigger: { type: 'once' } }]
-    })
-    await client.subscribe([1,2,3,4], 5)
-    await client.unsubscribe(3)
-    await client.process()
-    const res = await client.Tracks.findAll({ attributes: ['contact_id', 'campaign_id', 'action_id'], where: { campaign_id: client.id } }).then(res => res.map(res => res.toJSON()))
-    await client.Subs.destroy({ where: { campaign_id: client.id } })
-    await client.Tracks.destroy({ where: { campaign_id: client.id } })
-    expect(res).toEqual([{ contact_id: 1, campaign_id: 'test_once_2', action_id: 'action' }])
   })
 
   it('should unsubscribe', async () => {
@@ -95,7 +61,7 @@ describe(ClientClass.name, () => {
     await client.Subs.destroy({ where: { campaign_id: client.id } })
     const res2 = res1.map(({createdAt, ...row}) => row)
     const res3 = Object.keys(res1.reduce((map,item) => ({ ...map, [item.createdAt]: item.createdAt }), {}))
-    expect(res3.length).toEqual(3) //distributed over 3 diffrent days
+    expect(res3.length).toEqual(3) //distributed over 3 days
     expect(res2).toEqual([
       { contact_id: 1, active: true },
       { contact_id: 2, active: true },
@@ -111,11 +77,138 @@ describe(ClientClass.name, () => {
     await client.Subs.destroy({ where: { campaign_id: client.id } })
     const res2 = res1.map(({createdAt, ...row}) => row)
     const res3 = Object.keys(res1.reduce((map,item) => ({ ...map, [item.createdAt]: item.createdAt }), {}))
-    expect(res3.length).toEqual(1) //distributed over 1 diffrent days
+    expect(res3.length).toEqual(1) //distributed over 1 day
     expect(res2).toEqual([
       { contact_id: 1, active: true },
       { contact_id: 2, active: true },
       { contact_id: 3, active: true }
+    ])
+  })
+
+  it('should get subscribed contact ids', async () => {
+    const client = new ClientClass({ ...options, id: 'test_getSubscribedContactIds_1'})
+    await client.subscribe([1,2,3])
+    await client.unsubscribe(1)
+    const res = await client.getSubscribedContactIds()
+    expect(res).toEqual([2,3])
+  })
+
+  it('should trigger - once', async () => {
+    const res1 = []
+    const client = new ClientClass({ ...options, id: 'test_once_1',
+      callbacks: { testCallback: (contact, params) => res1.push({contact, params}) },
+      actions:[{ id: 'action', callback: 'testCallback', params: { msg: 'hello world' }, trigger: { type: 'once' } }]
+    })
+    await client.subscribe([1,2,3])
+    await client.unsubscribe(3)
+    await client.Tracks.bulkCreate([{ contact_id: 2, campaign_id: client.id, action_id: 'action' }])
+    await client.process()
+    await client.process()
+    await client.process()
+    const res2 = await client.Tracks.findAll({ attributes: ['contact_id', 'campaign_id', 'action_id'], where: { campaign_id: client.id } }).then(res => res.map(res => res.toJSON()))
+    await client.Subs.destroy({ where: { campaign_id: client.id } })
+    await client.Tracks.destroy({ where: { campaign_id: client.id } })
+    expect(res1).toEqual([
+      { contact: { uid: 1, email: 'julie@gmail.com', name: 'Julie Doe' }, params: { msg: 'hello world' } }
+    ])
+    expect(res2).toEqual([
+      { contact_id: 2, campaign_id: 'test_once_1', action_id: 'action' },
+      { contact_id: 1, campaign_id: 'test_once_1', action_id: 'action' }
+    ])
+  })
+
+  it('should trigger - once considering createdAt', async () => {
+    const client = new ClientClass({ ...options, id: 'test_once_2',
+      callbacks: { testCallback: (contact, params) => null },
+      actions:[{ id: 'action', callback: 'testCallback', params: null, trigger: { type: 'once' } }]
+    })
+    await client.subscribe([1,2,3,4], 5)
+    await client.unsubscribe(3)
+    await client.process()
+    await client.process()
+    await client.process()
+    const res = await client.Tracks.findAll({ attributes: ['contact_id', 'campaign_id', 'action_id'], where: { campaign_id: client.id } }).then(res => res.map(res => res.toJSON()))
+    await client.Subs.destroy({ where: { campaign_id: client.id } })
+    await client.Tracks.destroy({ where: { campaign_id: client.id } })
+    expect(res).toEqual([{ contact_id: 1, campaign_id: 'test_once_2', action_id: 'action' }])
+  })
+
+  it('should trigger - recurringly', async () => {
+    const res1 = []
+    const client = new ClientClass({ ...options, id: 'test_recurring_1',
+      callbacks: { testCallback: (contact, params) => res1.push({contact, params}) },
+      actions:[{ id: 'action', callback: 'testCallback', params: { msg: 'hello world' }, trigger: { type: 'recurring', intervalDays: 1 } }]
+    })
+
+    // seed
+    await client.subscribe([1,2,3])
+    await client.unsubscribe(3)
+    await client.Tracks.bulkCreate([{ contact_id: 2, campaign_id: client.id, action_id: 'action' }])
+
+    // run it
+    await client.process()
+    await client.process()
+    await client.process()
+
+    // inspect
+    const res2 = await client.Tracks.findAll({ attributes: ['contact_id', 'campaign_id', 'action_id'], where: { campaign_id: client.id } }).then(res => res.map(res => res.toJSON()))
+
+    // pastify dates
+    await client.Tracks.update({ createdAt: moment().subtract(1, 'days').toDate() }, { where: { campaign_id: client.id } })
+    await client.subscribe([3])
+
+    // run it
+    await client.process()
+    await client.process()
+    await client.process()
+
+    // inspect
+    const res3 = await client.Tracks.findAll({ attributes: ['contact_id', 'campaign_id', 'action_id'], where: { campaign_id: client.id } }).then(res => res.map(res => res.toJSON()))
+
+    // clean
+    await client.Subs.destroy({ where: { campaign_id: client.id } })
+    await client.Tracks.destroy({ where: { campaign_id: client.id } })
+
+    // assert
+    expect(res2).toEqual([
+      {
+        contact_id: 2,
+        campaign_id: 'test_recurring_1',
+        action_id: 'action'
+      },
+      {
+        contact_id: 1,
+        campaign_id: 'test_recurring_1',
+        action_id: 'action'
+      }
+    ])
+
+    expect(res3).toEqual([
+      {
+        contact_id: 2,
+        campaign_id: 'test_recurring_1',
+        action_id: 'action'
+      },
+      {
+        contact_id: 1,
+        campaign_id: 'test_recurring_1',
+        action_id: 'action'
+      },
+      {
+        contact_id: 1,
+        campaign_id: 'test_recurring_1',
+        action_id: 'action'
+      },
+      {
+        contact_id: 2,
+        campaign_id: 'test_recurring_1',
+        action_id: 'action'
+      },
+      {
+        contact_id: 3,
+        campaign_id: 'test_recurring_1',
+        action_id: 'action'
+      }
     ])
   })
 
